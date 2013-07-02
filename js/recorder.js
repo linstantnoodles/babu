e79c83533338b163e43936a4974ca311650cbc8b
Alan <linpd2@gmail.com>
2013-07-02 03:04:17 -0400

    removed some debugging stuff and moved variables into function scope

--- js/recorder.js
+++ js/recorder.js
@@ -1,21 +1,23 @@
 (function(window) {
 
-  var WORKER_PATH = 'js/recorderWorker.js',
-    audioContext = new webkitAudioContext(),
-    audioInput = null,
-    realAudioInput = null,
-    inputPoint = null,
-    audioRecorder = null,
-    bufferSrc = null,
-    rafID = null,
-    recIndex = 0,
-    recording = false;
+  'use strict';
 
   var Babu = function() {
-    var worker = new Worker(WORKER_PATH);
 
-    var self = this;
-    var currCallback = null;
+    var WORKER_PATH = 'js/recorderWorker.js',
+      audioContext = new webkitAudioContext(),
+      audioInput = null,
+      realAudioInput = null,
+      inputPoint = null,
+      audioRecorder = null,
+      bufferSrc = null,
+      rafID = null,
+      recIndex = 0,
+      bufferLen = 4096,
+      recording = false,
+      worker = new Worker(WORKER_PATH),
+      self = this,
+      currCallback = null;
 
     this.play = function(cb) {
       currCallback = cb || config.callback;
@@ -33,11 +35,8 @@
     }
 
     this.exportWAV = function(cb, type, action) {
-      console.log("export wav");
-      console.log(this);
       console.log("exporting WAV");
       currCallback = cb;
-      console.log(cb);
       type = type || 'audio/wav';
       if (!currCallback) throw new Error('Callback not set');
       worker.postMessage({
@@ -48,20 +47,8 @@
     }
 
     worker.onmessage = function(e) {
-      console.log("Worker");
-      console.log(this);
       var blob = e.data;
       currCallback(blob);
-    }
-
-    this.force = function(blob, filename) {
-      var url = (window.URL || window.webkitURL).createObjectURL(blob);
-      var link = window.document.createElement('a');
-      link.href = url;
-      link.download = filename || 'output.wav';
-      var click = document.createEvent("Event");
-      click.initEvent("click", true, true);
-      link.dispatchEvent(click);
     }
 
     this.upload = function() {
@@ -70,13 +57,11 @@
     }
 
     this.download = function() {
-      console.log(this);
       this.exportWAV(this.doneEncoding, null, 'download');
     }
 
     this.playAudio = function() {
       console.log('Playing');
-      console.log(this);
       this.play(this.setBuffers);
     }
 
@@ -85,18 +70,20 @@
       bufferSrc.stop(0);
     }
 
-    this.test = function() {
-      alert("WOW");
-    }
     // Callback function for exportWAV
     this.doneEncoding = function(blob) {
       var filename = "myRecording" + ((recIndex < 10) ? "0" : "") + recIndex + ".wav";
-      self.force(blob, filename);
+      var url = (window.URL || window.webkitURL).createObjectURL(blob);
+      var link = window.document.createElement('a');
+      link.href = url;
+      link.download = filename || 'output.wav';
+      var click = document.createEvent("Event");
+      click.initEvent("click", true, true);
+      link.dispatchEvent(click);
       recIndex++;
     }
 
     this.setBuffers = function(buffers) {
-      console.log(this);
       bufferSrc = audioContext.createBufferSource();
       bufferSrc.buffer = audioContext.createBuffer(1, buffers[0].length, 44100);
       bufferSrc.buffer.getChannelData(0).set(buffers[0]);
@@ -130,11 +117,9 @@
       audioInput.connect(inputPoint);
       // Pipe buffer data into audio destination (speakers)
       bufferSrc.connect(audioContext.destination);
-      // Connects gain node to recorder
-      var bufferLen = 4096;
-      //creating script processor node
+      // Creating script processor node
       this.node = audioContext.createJavaScriptNode(bufferLen, 2, 2);
-      //create a web worker that contains implementation of buffer manipulation and WAV encoding
+      // Create a web worker that contains implementation of buffer manipulation and WAV encoding
       worker.postMessage({
         command: 'init',
         config: {
@@ -142,11 +127,10 @@
           uploadUrl: 'http://localhost/babu/test.php',
         }
       });
-
+      // Listen for record event
       this.node.onaudioprocess = function(e) {
-        //this event is dispatched by the one in main
         if (!recording) return;
-        console.log("record");
+        console.log("recording");
         worker.postMessage({
           command: 'record',
           buffer: [
@@ -159,14 +143,9 @@
       // Pipe the first gain node to recorder
       inputPoint.connect(this.node);
       // Pipe recorder output to audio destination
-      this.node.connect(audioContext.destination); //this should not be necessary
-      zeroGain = audioContext.createGainNode();
-      zeroGain.gain.value = 0.0;
-      inputPoint.connect(zeroGain);
-      zeroGain.connect(audioContext.destination);
+      this.node.connect(audioContext.destination);
     }
 
-    //gets called
     this.initAudio = function() {
       if (!navigator.webkitGetUserMedia)
         return (alert("Error: getUserMedia not supported!"));
